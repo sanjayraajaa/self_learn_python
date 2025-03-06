57f006b7f77450dc244e9cb8462bb252ec859298
Sanjay Raja <thesanjayraja@gmail.com>
2025-03-06 23:23:07 +0530

    Day 3

--- 01_class/practice.py
+++ 01_class/practice.py
@@ -1,19 +1,27 @@
-class Student():
-    def __init__(self,name):
-        self._name = name
-        self.__grade = []
+class User():
+    def __init__(self,user,name):
+        self.user = user
+        self.name = name
     
-    def add_grade(self, grade):
-        self.__grade.append(grade)
+    def show_user_details(self):
+        print(f"User Details:\n---- --------\nUser: {self.user}\nName: {self.name}\n")
+
+class Employee():
+    def __init__(self,emp_id,dept):
+        self.emp_id = emp_id
+        self.dept = dept
     
-    def get_average(self):
-        average = sum(self.__grade) / len(self.__grade)
-        return average
+    def show_employee_details(self):
+        print(f"Employee Details:\n-------- --------\nEmployee ID: {self.emp_id}\nDepartment: {self.dept}")
 
-a = Student("Sanjay")
-a.add_grade(50)
-a.add_grade(100)
-a.add_grade(50)
-a.add_grade(100)
+class HRMS(User,Employee):
+    def __init__(self, user, name,emp_id,dept):
+        User.__init__(self, user, name)
+        Employee.__init__(self,emp_id,dept)
+    
+    def show_details(self):
+        User.show_user_details(self)
+        Employee.show_employee_details(self)
 
-print(a.get_average())
+u1 = HRMS("sanjayraja","Sanjay Raja S","HR-EMP-0001","Frappe")
+u1.show_details()
